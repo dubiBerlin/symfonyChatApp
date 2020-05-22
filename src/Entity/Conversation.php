71f8fd52d6acc07ee64c8997c51a72310ea7ee89
DubiBerlin <woose@web.de>
2020-05-22 23:03:39 +0200

    add props to Conversation Entity

--- src/Entity/Conversation.php
+++ src/Entity/Conversation.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ConversationRepository::class)
+ * @ORM\Table(indexes={@Index(name="last_message_id_index", columns={"last_message_id"})})
  */
 class Conversation
 {
@@ -16,6 +17,23 @@ class Conversation
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Participant", mappedBy="conversation")
+     */
+    private $participants;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Message")
+     * @ORM\JoinColumn(name="last_message_id", referencedColumnName="id")
+     */
+    private $lastMessage;
+
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Message", mappedBy="conversation")
+     */
+    private $message;
 
     public function getId(): ?int
     {
