7652534c0addb5cdff4c9990681623545ac060b1
DubiBerlin <woose@web.de>
2020-05-22 23:24:16 +0200

    add props to Message Entity, create Timestamp Entity

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -7,15 +7,37 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=MessageRepository::class)
+ * @ORM\Table(indexes={@Index(name="created_at_index", columns={"created_at"})})
+ * @ORM\HasLifecycleCallbacks()
  */
 class Message
 {
+    use Timestamp;
+
+
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="messages")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Conversation", inversedBy="messages")
+     */
+    private $conversation;
+
 
     public function getId(): ?int
     {
