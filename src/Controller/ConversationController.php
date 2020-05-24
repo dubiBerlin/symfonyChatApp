9f3651dd63743ae2730f202a6dbfdb2da947104e
DubiBerlin <woose@web.de>
2020-05-24 22:47:04 +0200

    ConversationController

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -4,16 +4,61 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use App\Repository\ConversationRepository;
 
+
+/**
+ * @Route("/conversations", name="conversations")
+ */
 class ConversationController extends AbstractController
 {
-    /**
-     * @Route("/conversation", name="conversation")
-     */
-    public function index()
+
+    private $userRepository;
+
+    private $entityManager;
+
+    private $conversationRepository;
+
+    public function __construct(
+      UserRepository $userRepository, 
+      EntityManagerInterface $entityManager, 
+      ConversationRepository $conversationRepository
+    ) 
     {
-        return $this->render('conversation/index.html.twig', [
-            'controller_name' => 'ConversationController',
-        ]);
+      $this->userRepository = $userRepository; 
+      $this->entityManager = $entityManager;
+      $this->conversationRepository = $conversationRepository;
+    }
+
+    /**
+     * @Route("/", name="getConversation")
+     * @param Request $request#
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $otherUser = $request->get("otherUser");
+        $otherUser = $this->userRepository->find($otherUser);
+
+        if (is_null($otherUser)) {
+          throw new \Exception("The user was not found");
+        }
+
+        // cannot create conversation with myself
+        if ($otherUser->getId() === $this->getUser()->getId()) {
+          throw new \Exception("Cant create a conversation with yourself");
+        }
+
+        // Check if conversation already exists
+        $conversation = $this->conversationRepository->findConversationByParticipants(
+          $otherUser->getId(),
+          $this->getUser()->getId()
+        );
+
+        return $this->json();
     }
 }
